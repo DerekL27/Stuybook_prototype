c996467975135e82ca0c0209cb348a1557802f46
Derek Leung <dleung00@stuy.edu>
2020-01-08 14:53:39 -0500

    did database creation stuff

--- utl/dbfunc.py
+++ utl/dbfunc.py
@@ -2,7 +2,11 @@ import urllib.request as request
 import json
 
 def setup(c):
-    c.execute('CREATE TABLE IF NOT EXISTS users (userID INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, username TEXT, password TEXT, displayname TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS users (userID INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, username TEXT, password TEXT, displayName TEXT, scheduleIndex INTEGER FOREIGN KEY)')
+    c.execute('CREATE TABLE IF NOT EXISTS schedules (index INTEGER PRIMARY KEY, schedule BLOB)')
+    c.execute('CREATE TABLE IF NOT EXISTS groups (index INTEGER PRIMARY KEY, groupName TEXT, posts BLOB, members BLOB)')
+    c.execute('CREATE TABLE IF NOT EXISTS posts (postIndex INTEGER PRIMARY KEY, author INTEGER, words TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS leaderboards (userID INTEGER PRIMARY KEY, superheroScore INTEGER, anagramScore INTEGER, triviaScore INTEGER)')
 
 def createUser(c, username, password, displayname, email):
-    c.execute("INSERT INTO users VALUES (NULL, '%s', '%s', '%s', '%s')" % (email, username, password, displayname))
+    c.execute("INSERT INTO users VALUES(NULL, '%s', '%s', '%s', '%s')" % (email, username, password, displayname))
