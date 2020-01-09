2a80a0e5e33999ba606b10efdf3de3798b593657
Derek Leung <dleung00@stuy.edu>
2020-01-09 14:56:58 -0500

    derek is so cool

--- utl/dbfunc.py
+++ utl/dbfunc.py
@@ -2,12 +2,18 @@ import urllib.request as request
 import json
 
 def setup(c):
-    c.execute('CREATE TABLE IF NOT EXISTS users (userID INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT, username TEXT, password TEXT, displayName TEXT, scheduleIndex INTEGER FOREIGN KEY)')
-    c.execute('CREATE TABLE IF NOT EXISTS schedules (index INTEGER PRIMARY KEY, schedule BLOB)')
-    c.execute('CREATE TABLE IF NOT EXISTS groups (index INTEGER PRIMARY KEY, groupName TEXT, posts BLOB, members BLOB)')
+    c.execute('CREATE TABLE IF NOT EXISTS users (userID INTEGER PRIMARY KEY, email TEXT, username TEXT, password TEXT, displayName TEXT)')
+    c.execute('CREATE TABLE IF NOT EXISTS schedules (scheduleID INTEGER PRIMARY KEY, schedule BLOB)')
+    c.execute('CREATE TABLE IF NOT EXISTS groups (groupID INTEGER PRIMARY KEY, groupName TEXT, posts BLOB, members BLOB)')
     c.execute('CREATE TABLE IF NOT EXISTS posts (postIndex INTEGER PRIMARY KEY, author INTEGER, words TEXT)')
     c.execute('CREATE TABLE IF NOT EXISTS leaderboards (userID INTEGER PRIMARY KEY, superheroScore INTEGER, anagramScore INTEGER, triviaScore INTEGER)')
 
+def countRows(table):
+    c.execute("SELECT COUNT(*) FROM {}".format(table))
+    return c.fetchall()[0]
+
+#c is the cursor being used
 def createUser(c, username, password, displayname, email):
-    c.execute("INSERT INTO users VALUES(NULL, '%s', '%s', '%s', '%s')" % (email, username, password, displayname))
-    c.edecute("INSERT INTO schedules VALUES()")
+    nextIndex = countRows("users")
+    c.execute("INSERT INTO userdata VALUES (?, ?, ?, ?, ?)",(nextIndex, email, username, password, displayname))
+    c.execute("INSERT INTO schedules VALUES(?, ?),(nextIndex,[null,null,null,null,null,null,null,null,null,null]) ")
