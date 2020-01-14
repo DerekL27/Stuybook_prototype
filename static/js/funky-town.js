0fa47496774e8a267bee43ae88d1def614877a45
grace-mao23 <gmao00@stuy.edu>
2020-01-14 00:30:01 -0500

    Javascript progress

---
+++ static/js/funky-town.js
@@ -0,0 +1,25 @@
+var a = document.getElementById('new-post')
+var func = function() {
+  // text box
+  var div = document.createElement("div");
+  div.className = "form-horizontal"
+  div.innerHTML = `
+    <div class="form-group">
+        <div class="col-md-6">
+            <textarea class="form-control" rows="3" placeholder="What's up?" required></textarea>
+        </div>
+    </div>
+  `;
+  document.getElementById("feed").appendChild(div);
+
+  var button = document.createElement("a");
+  button.type = "button";
+  button.className = "btn btn-success ml-3";
+  button.innerHTML = "Post!";
+  button.href = "/posting";
+  document.getElementById("feed").appendChild(button);
+  /*var thing = document.createElement("div");
+  thing.innerText = "Hello World";
+  document.getElementById("feed").appendChild(thing);*/
+}
+a.addEventListener('click', func);
