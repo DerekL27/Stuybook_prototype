f2b76f27419a066846fce2f941c6a09e784f43fd
grace-mao23 <gmao00@stuy.edu>
2020-01-14 23:22:26 -0500

    js'

--- static/js/funky-town.js
+++ static/js/funky-town.js
@@ -1,22 +1,3 @@
-var bro = document.getElementById('editbio')
-var another = function() {
-  var div = document.createElement("div");
-  div.className = "form-horizontal"
-  var place = document.getElementById('currentbio').innerHTML
-  div.innerHTML = `
-    <form action='/edit_bio' method='POST'>
-    <div class="form-group">
-        <div class="col-md-10">
-            <textarea class="form-control" rows="3" placeholder="Something new" name="newbio" required></textarea>
-        </div>
-    </div>
-    <button type="submit" class="btn btn-warning ml-3">Save</button>
-    </form>
-  `;
-  document.getElementById("editbox").appendChild(div);
-}
-bro.addEventListener('click', another);
-
 var a = document.getElementById('new-post')
 var func = function() {
   // text box
@@ -40,28 +21,21 @@ var func = function() {
 }
 a.addEventListener('click', func);
 
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  //var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  //span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
-    }
-  }
+var bro = document.getElementById('editbio')
+var another = function() {
+  var div = document.createElement("div");
+  div.className = "form-horizontal"
+  var place = document.getElementById('currentbio').innerHTML
+  div.innerHTML = `
+    <form action='/edit_bio' method='POST'>
+    <div class="form-group">
+        <div class="col-md-10">
+            <textarea class="form-control" rows="3" placeholder="Something new" name="newbio" required></textarea>
+        </div>
+    </div>
+    <button type="submit" class="btn btn-warning ml-3">Save</button>
+    </form>
+  `;
+  document.getElementById("editbox").appendChild(div);
 }
+bro.addEventListener('click', another);
